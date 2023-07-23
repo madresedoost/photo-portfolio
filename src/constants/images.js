21200c21c63bb2a9f61ca864dc748f65a2f80173
Ifeanyi Aladi <benedictaladi@gmail.com>
2023-07-23 23:11:59 +0100

    updated files.

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,5 @@
 import bg from '../assets/bg.png';
 import ceo from '../assets/ceo.jpg';
-import G from '../assets/G.png';
 import gallery01 from '../assets/gallery01.jpg';
 import gallery02 from '../assets/gallery02.jpg';
 import gallery03 from '../assets/gallery03.jpg';
@@ -11,12 +10,6 @@ import camera from '../assets/camera.png';
 import services from '../assets/services-img.jpg';
 import overlaybg from '../assets/overlaybg.png';
 import welcome from '../assets/welcome.jpg';
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import logo from '../assets/logo.png';
@@ -24,7 +17,6 @@ import logo from '../assets/logo.png';
 export default {
   bg,
   ceo,
-  G,
   services,
   gallery01,
   gallery02,
@@ -35,12 +27,6 @@ export default {
   gallery05,
   overlaybg,
   welcome,
-  findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
   sign,
   quote,
   logo,
